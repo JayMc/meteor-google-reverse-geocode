c994aeaa78592d80eb606a5714c66f886f105b92
Jason McIver <mciver.jason@gmail.com>
2015-03-23 11:12:44 +1100

    first commit

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'jaymc:google-reverse-geocode',
+  version: '0.0.1',
+  summary: 'Calls Googles reverse geocode API converting GPS lat and lng to physical address',
+  git: 'https://github.com/JayMc/meteor-google-reverse-geocode',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.4.1');
+  api.use([
+    'http'
+  ]);
+  api.addFiles('google-reverse-geocode.js');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('jaymc:google-reverse-geocode');
+  api.addFiles('google-reverse-geocode-tests.js');
+});
